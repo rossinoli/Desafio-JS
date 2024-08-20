0fe6f96d2fd2f979cd48fd5df3d1b0357614e4ed
Thaina Leticia Rossinoli <103609562+rossinoli@users.noreply.github.com>
2024-08-20 17:58:05 -0300

    documentação pronta

--- TodoNow/assets/js/script.js
+++ TodoNow/assets/js/script.js
@@ -1,19 +1,35 @@
 function adicionarTarefa() {
+    //pega o texto digitado e armazena na novaTarefa
     const novaTarefa = document.getElementById('novaTarefa').value;
+
+    //validando se string esta vazio, antes de armazenar 
     if (novaTarefa === '') return;
   
+    // armazemar como lista oq foi escrito
     const lista = document.getElementById('listaTarefas');
+
+    //cria um novo li
     const itemLista = document.createElement('li');
+
+    //atribui texto na lista
     itemLista.textContent = novaTarefa;
   
+    //criação do botao concluir
     const botaoConcluir = document.createElement('button');
     botaoConcluir.textContent = 'Concluir';
+
+    //click, a função definida é executada.
     botaoConcluir.onclick = function() {
+
+    //estilo do item da lista é alterado, indicando que a tarefa foi concluída
       itemLista.classList.toggle('concluida');
     };
   
+    //adiciona o botao concluir dentro da lista
     itemLista.appendChild(botaoConcluir);
     lista.appendChild(itemLista);
-  
+
+    // deixa o input nova tarefa string vazio
     document.getElementById('novaTarefa').value = '';
-  }
+
+}
