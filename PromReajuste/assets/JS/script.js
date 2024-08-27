d2e505c0bb0841df2ac11176d9f23ed133f6d2b3
Thaina Leticia Rossinoli <103609562+rossinoli@users.noreply.github.com>
2024-08-27 16:50:40 -0300

    Update script.js

--- PromReajuste/assets/JS/script.js
+++ PromReajuste/assets/JS/script.js
@@ -1,27 +1,28 @@
 function calcularReajuste() {
-    // Obtém o valor do salário digitado
-    const salarioAtual = parseFloat(document.getElementById('salario').value);
-  
-    // Define as faixas salariais e seus respectivos percentuais de aumento
-    const faixasSalariais = [
-      { faixa: 280, percentual: 20 },
-      { faixa: 700, percentual: 15 },
-      { faixa: 1500, percentual: 10 },
-      { faixa: Infinity, percentual: 5 }, 
-    ];
-  
-    // Encontra a faixa salarial correspondente
-    const faixa = faixasSalariais.find(f => salarioAtual <= f.faixa);
-  
-    // Calcula o valor do aumento e o novo salário
-    const valorAumento = salarioAtual * (faixa.percentual / 100);
-    const novoSalario = salarioAtual + valorAumento;
-  
-    // Exibe o resultado
-    const resultadoElement = document.getElementById('resultado');
-    resultadoElement.textContent = `
-    Salário antes do reajuste: R$ ${salarioAtual.toFixed(2)}
-    - Novo salário: R$ ${novoSalario.toFixed(2)}
-    - Aumento de: ${faixa.percentual}%
-    `;
+  // Obtém o valor do salário digitado
+  const salarioAtual = parseFloat(document.getElementById('salario').value);
+
+  let novoSalario, percentual;
+
+  if (salarioAtual <= 280) {
+    percentual = 20;
+  } else if (salarioAtual <= 700) {
+    percentual = 15;
+  } else if (salarioAtual <= 1500) {
+    percentual = 10;
+  } else {
+    percentual = 5;
   }
+
+  // Calcula o valor do aumento e o novo salário
+  const valorAumento = salarioAtual * (percentual / 100);
+  novoSalario = salarioAtual + valorAumento;
+
+  // Exibe o resultado
+  const resultadoElement = document.getElementById('resultado');
+  resultadoElement.textContent = `
+  Salário antes do reajuste: R$ ${salarioAtual.toFixed(2)}
+  - Novo salário: R$ ${novoSalario.toFixed(2)}
+  - Aumento de: ${percentual}%
+  `;
+}
